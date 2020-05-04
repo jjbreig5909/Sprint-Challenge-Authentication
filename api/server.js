bd83868635a17eb96892f589468359e26177c01c
unknown <jjbreig@gmail.com>
2020-05-03 21:46:49 -0400

    added login and register, get works

--- api/server.js
+++ api/server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const session = require("express-session");
+const knexSessionStore = require("connect-session-knex")(session);
 const cors = require('cors');
 const helmet = require('helmet');
 
@@ -8,9 +10,32 @@ const jokesRouter = require('../jokes/jokes-router.js');
 
 const server = express();
 
+const sessionConfig = {
+  name: "chocolate-chip",
+  secret: "myspeshulsecret",
+  cookie: {
+    maxAge: 3600 * 1000,
+    secure: false, // should be true in production
+    httpOnly: true,
+  },
+  resave: false,
+  saveUninitialized: false,
+
+  store: new knexSessionStore({
+    knex: require("../database/dbConfig.js"),
+    tablename: "sessions",
+    sidfieldname: "sid",
+    createtable: true,
+    clearInterval: 3600 * 1000,
+  }),
+};
+
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
+
+server.use(session(sessionConfig));
+
 
 server.use('/api/auth', authRouter);
 server.use('/api/jokes', authenticate, jokesRouter);
--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,40 @@
 const router = require('express').Router();
-
+const bcrypt = require('bcryptjs');
+const Users = require("./auth-model.js")
 router.post('/register', (req, res) => {
-  // implement registration
+
+  const user = req.body;
+  const hash = bcrypt.hashSync(user.password, 8);
+  user.password = hash;
+
+  Users.add(user)
+    .then(saved => {
+      res.status(201).json(saved);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
 });
 
 router.post('/login', (req, res) => {
-  // implement login
+  let { username, password } = req.body;
+
+
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        console.log(username)
+        req.session.user = username;
+        res.status(200).json({ message: `Welcome ${user.username}!`, });
+      } else {
+        res.status(401).json({ message: 'invalid credentials' });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+      console.log(error)
+    });
 });
 
 module.exports = router;
